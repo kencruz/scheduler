28494ade5a5f97dbd00f91c9f9e5b0b7e98ef189
Ken Cruz <ken@kencruz.ca>
2021-12-09 21:11:35 -0500

    used api for getting days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-
-const days = [
-    {
-          id: 1,
-          name: "Monday",
-          spots: 2,
-        },
-    {
-          id: 2,
-          name: "Tuesday",
-          spots: 5,
-        },
-    {
-          id: 3,
-          name: "Wednesday",
-          spots: 0,
-        },
-];
 
 const appointments = [
   {
@@ -31,12 +14,12 @@ const appointments = [
     time: "1pm",
     interview: {
       student: "Lydia Miller-Jones",
-      interviewer:{
+      interviewer: {
         id: 3,
         name: "Sylvia Palmer",
         avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
+      },
+    },
   },
   {
     id: 3,
@@ -47,50 +30,53 @@ const appointments = [
     time: "3pm",
     interview: {
       student: "Archie Andrews",
-      interviewer:{
+      interviewer: {
         id: 4,
         name: "Cohana Roy",
         avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
+      },
+    },
   },
   {
     id: 5,
     time: "4pm",
-  }
+  },
 ];
-
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
-  
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8001/api/days")
+      .then((res) => {
+        setDays([...res.data]);
+      })
+      .catch((err) => console.error(err.message));
+  }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
-    <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-    <nav className="sidebar__menu">
-    <DayList
-      days={days}
-      value={day}
-      onChange={day => setDay(day)}
-    />
-
-    </nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
-
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList days={days} value={day} onChange={(day) => setDay(day)} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        { appointments.map(a => { 
-          return (<Appointment key={a.id} {...a} />);
+        {appointments.map((a) => {
+          return <Appointment key={a.id} {...a} />;
         })}
       </section>
     </main>
