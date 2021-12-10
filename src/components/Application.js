19af0eea1883b852b498d5f0993b174f57fecad2
Ken Cruz <ken@kencruz.ca>
2021-12-09 23:42:42 -0500

    get appointments from api

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,23 +3,27 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
+import { getAppointmentsForDay } from "../helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: [],
+    appointments: {},
   });
-  const setDay = (day) => setState({ ...state, day });
-  const setDays = (days) => setState((prev) => ({ ...prev, days }));
+  const setDay = (day) => {
+    setState({ ...state, day });
+  };
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   useEffect(() => {
-    axios
-      .get("http://localhost:8001/api/days")
-      .then((res) => {
-        setDays(res.data);
-      })
-      .catch((err) => console.error(err.message));
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+    ]).then((all) => {
+      const [days, appointments] = [all[0].data, all[1].data];
+      setState((prev) => ({ ...prev, days, appointments }));
+    });
   }, []);
 
   return (
@@ -45,7 +49,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {state.appointments.map((a) => {
+        {dailyAppointments.map((a) => {
           return <Appointment key={a.id} {...a} />;
         })}
       </section>
