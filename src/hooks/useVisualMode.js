0698cb92d148dec22753e6220aa4522914e1fc74
Ken Cruz <ken@kencruz.ca>
2021-12-11 16:30:05 -0500

    refactored useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,22 +6,21 @@ export function useVisualMode(initial) {
 
   function transition(newMode, replace) {
     if (replace) {
-      if (history.length > 1) {
-        const newHistory = history.slice(0, history.length - 1);
-        setHistory([...newHistory, newMode]);
-        setMode(newMode);
-      }
-    } else {
-      setHistory([...history, newMode]);
-      setMode(newMode);
+      back();
     }
+    setHistory((prev) => {
+      setMode(() => newMode);
+      return [...prev, newMode];
+    });
   }
 
   function back() {
     if (history.length > 1) {
-      const newHistory = history.slice(0, history.length - 1);
-      setHistory(() => newHistory);
-      setMode(() => newHistory[newHistory.length - 1]);
+      setHistory((prev) => {
+        const newHistory = prev.slice(0, prev.length - 1);
+        setMode(() => newHistory[newHistory.length - 1]);
+        return newHistory;
+      });
     }
   }
   return { transition, mode, back };
