d8eb382e9b08b4f5fdd84017f47d35ddabc6aaaa
Ken Cruz <ken@kencruz.ca>
2021-12-10 22:16:10 -0500

    implemented useVisualMode hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+export function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace) {
+    if (replace) {
+      if (history.length > 1) {
+        const newHistory = history.slice(0, history.length - 1);
+        setHistory([...newHistory, newMode]);
+        setMode(newMode);
+      }
+    } else {
+      setHistory([...history, newMode]);
+      setMode(newMode);
+    }
+  }
+
+  function back() {
+    if (history.length > 1) {
+      const newHistory = history.slice(0, history.length - 1);
+      setHistory(() => newHistory);
+      setMode(() => newHistory[newHistory.length - 1]);
+    }
+  }
+  return { transition, mode, back };
+}
