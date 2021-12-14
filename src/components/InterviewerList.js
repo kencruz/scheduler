ee5f35f527490beb1b0da830242f32a1fe0fe772
Ken Cruz <ken@kencruz.ca>
2021-12-14 15:36:18 -0500

    added prop-types as a dependency

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,22 +1,27 @@
 import React from "react";
-import "components/InterviewerList.scss"
-import InterviewerListItem from "components/InterviewerListItem"
+import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
+import PropTypes from "prop-types";
 
 export default function InterviewerList(props) {
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
-        { props.interviewers.map(
-            i => <InterviewerListItem 
-              key={i.id} 
-              setInterviewer={() => props.onChange(i.id)} 
-              name={i.name} 
-              avatar={i.avatar} 
-              selected={props.value === i.id} 
-            /> )
-        }
+        {props.interviewers.map((i) => (
+          <InterviewerListItem
+            key={i.id}
+            setInterviewer={() => props.onChange(i.id)}
+            name={i.name}
+            avatar={i.avatar}
+            selected={props.value === i.id}
+          />
+        ))}
       </ul>
     </section>
-  )
+  );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
